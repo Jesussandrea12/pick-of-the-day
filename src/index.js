1e2b931416886190a3e424ec9f3fca7a8550ca67
Jesus Sandrea <jesussandrea12@gmail.com>
2018-12-17 14:50:12 -0500

    conflict bs

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import { render } from 'react-dom';
 import Router from './components/Router';
 import './css/style.css';
 
-render(<Router/>, document.getElementById('main'));
+render(<Router />, document.getElementById('main'));
